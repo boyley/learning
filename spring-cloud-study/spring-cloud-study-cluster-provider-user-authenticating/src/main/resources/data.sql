233c084aa74403951f52e0b27d514d237744f41e
lenovo <136397156@qq.com>
2017-05-26 22:04:45 +0800

    需要认证的eureka

---
+++ spring-cloud-study/spring-cloud-study-cluster-provider-user-authenticating/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (id,username,name,age,balance) values (1,'account1','张三',20,100.00);
+INSERT INTO user (id,username,name,age,balance) values (2,'account2','李四',28,140.00);
+INSERT INTO user (id,username,name,age,balance) values (3,'account3','王五',30,150.00);
+INSERT INTO user (id,username,name,age,balance) values (4,'account4','三八',37,160.00);
+INSERT INTO user (id,username,name,age,balance) values (5,'account5','四起',37,170.00);
+INSERT INTO user (id,username,name,age,balance) values (6,'account6','五三',40,190.00);
