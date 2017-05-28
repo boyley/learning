2a5e8468f8eb43b34f55bfd719a65ac5e0f44bd6
lenovo <136397156@qq.com>
2017-05-28 15:16:00 +0800

    脱离Eureka使用Ribbon

---
+++ spring-cloud-study/spring-cloud-study-ribbon-user-without-eureka/src/main/java/cn/shagle/learning/controller/UserController.java
@@ -0,0 +1,39 @@
+package cn.shagle.learning.controller;
+
+import cn.shagle.learning.dto.User;
+import cn.shagle.learning.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Danlu on 2017/5/12.
+ */
+@RestController
+public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/ribbon/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userService.findById(id);
+    }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance() {
+        // 打印当前选择的是哪个节点
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("provider-user");
+        logger.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+    }
+}
---
+++ spring-cloud-study/spring-cloud-study-ribbon-user-without-eureka/src/main/java/cn/shagle/learning/dto/User.java
@@ -0,0 +1,56 @@
+package cn.shagle.learning.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * Created by Danlu on 2017/5/12.
+ */
+public class User implements Serializable {
+
+    private int id;
+    private String username;
+    private String name;
+    private int age;
+    private BigDecimal balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
