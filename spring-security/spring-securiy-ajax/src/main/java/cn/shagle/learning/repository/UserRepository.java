c48d339c620251be842ff4bde79796aa5e6f25a0
zhaobo@danlu.com <12345678>
2017-06-15 15:06:51 +0800

    spring security ajax

---
+++ spring-security/spring-securiy-ajax/src/main/java/cn/shagle/learning/repository/UserRepository.java
@@ -0,0 +1,37 @@
+package cn.shagle.learning.repository;
+
+import cn.shagle.learning.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by Danlu on 2017/6/15.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findOneByActivationKey(String activationKey);
+
+    List<User> findAllByActivatedIsFalseAndCreatedDateBefore(Instant dateTime);
+
+    Optional<User> findOneByResetKey(String resetKey);
+
+    Optional<User> findOneByEmail(String email);
+
+    Optional<User> findOneByLogin(String login);
+
+    @EntityGraph(attributePaths = "authorities")
+    User findOneWithAuthoritiesById(Long id);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByLogin(String login);
+
+    Page<User> findAllByLoginNot(Pageable pageable, String login);
+}
