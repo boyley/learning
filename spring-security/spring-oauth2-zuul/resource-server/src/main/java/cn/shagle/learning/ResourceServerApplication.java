aec0c67fc0bae238a416bdfd869b9f4ca575c840
lenovo <136397156@qq.com>
2017-06-20 23:26:06 +0800

    1. spring zuul security

---
+++ spring-security/spring-oauth2-zuul/resource-server/src/main/java/cn/shagle/learning/ResourceServerApplication.java
@@ -0,0 +1,17 @@
+package cn.shagle.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Filip Lindby
+ *
+ */
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+}
---
+++ spring-security/spring-oauth2-zuul/ui1/src/main/java/cn/shagle/learning/Ui1Application.java
@@ -0,0 +1,17 @@
+package cn.shagle.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Filip Lindby
+ *
+ */
+@SpringBootApplication
+public class Ui1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ui1Application.class, args);
+	}
+}
