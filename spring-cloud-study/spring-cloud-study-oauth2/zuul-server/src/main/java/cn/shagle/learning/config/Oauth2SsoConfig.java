7b96b438c9eb87a12bea1b5a279ca3a8a580b4f6
zhaobo@danlu.com <12345678>
2017-07-07 16:28:56 +0800

    spring security context save to redis

--- spring-cloud-study/spring-cloud-study-oauth2/zuul-server/src/main/java/cn/shagle/learning/config/Oauth2SsoConfig.java
+++ spring-cloud-study/spring-cloud-study-oauth2/zuul-server/src/main/java/cn/shagle/learning/config/Oauth2SsoConfig.java
@@ -11,6 +11,9 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
@@ -28,6 +31,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.UUID;
 
 /**
  * Created by lenovo on 2017/7/6.
@@ -56,12 +61,66 @@ public class Oauth2SsoConfig extends OAuth2SsoDefaultConfiguration {
                 .csrfTokenRepository(csrfTokenRepository())
                 .and()
                 .securityContext().securityContextRepository(securityContextRepository()).and()
-                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+                .addFilterBefore(tokenFilter(),SecurityContextPersistenceFilter.class);
     }
 
     @Bean
     public SecurityContextRepository securityContextRepository() {
         return new RedisSecurityContextRepository();
+    }
+
+    private Filter tokenFilter() {
+        return new OncePerRequestFilter() {
+
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                String tokenValue = extractToken(request);
+                if(tokenValue == null) {
+                    request.setAttribute("access_token", UUID.randomUUID().toString());
+                }
+                filterChain.doFilter(request, response);
+            }
+
+
+            private String extractToken(HttpServletRequest request) {
+                // first check the header...
+                String token = extractHeaderToken(request);
+
+                // bearer type allows a request parameter as well
+                if (token == null) {
+                    logger.debug("Token not found in headers. Trying request parameters.");
+                    token = request.getParameter(OAuth2AccessToken.ACCESS_TOKEN);
+                    if (token == null) {
+                        logger.debug("Token not found in request parameters.  Not an OAuth2 request.");
+                    } else {
+                        request.setAttribute(OAuth2AuthenticationDetails.ACCESS_TOKEN_TYPE, OAuth2AccessToken.BEARER_TYPE);
+                    }
+                }
+
+                return token;
+            }
+
+            private String extractHeaderToken(HttpServletRequest request) {
+                Enumeration<String> headers = request.getHeaders("Authorization");
+                while (headers.hasMoreElements()) { // typically there is only one (most servers enforce that)
+                    String value = headers.nextElement();
+                    if ((value.toLowerCase().startsWith(OAuth2AccessToken.BEARER_TYPE.toLowerCase()))) {
+                        String authHeaderValue = value.substring(OAuth2AccessToken.BEARER_TYPE.length()).trim();
+                        // Add this here for the auth details later. Would be better to change the signature of this method.
+                        request.setAttribute(OAuth2AuthenticationDetails.ACCESS_TOKEN_TYPE,
+                                value.substring(0, OAuth2AccessToken.BEARER_TYPE.length()).trim());
+                        int commaIndex = authHeaderValue.indexOf(',');
+                        if (commaIndex > 0) {
+                            authHeaderValue = authHeaderValue.substring(0, commaIndex);
+                        }
+                        return authHeaderValue;
+                    }
+                }
+
+                return null;
+            }
+        };
     }
 
     private Filter csrfHeaderFilter() {
